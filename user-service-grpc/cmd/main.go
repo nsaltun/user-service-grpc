0851c1fad58cdfff4db39657894adac1c417aa8f
nsaltun <enal25@hotmail.com>
2024-12-10 23:11:39 +0300

    user proto gen with go mod init

--- user-service-grpc/cmd/main.go
+++ user-service-grpc/cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Println("hello world")
+	server := grpc.NewServer()
+
+	//Register userapi to server
+
+	listener, err := net.Listen("tcp", "50051")
+	if err != nil {
+		log.Fatalf("Failed to listen. %v", err)
+	}
+
+	if err := server.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve. %v", err)
+	}
+
 }
