d6b330ee7fa847fa7283dc1145d224e779d0daeb
nsaltun <enal25@hotmail.com>
2024-12-11 13:10:43 +0300

    added grpc reflection

--- user-service-grpc/cmd/main.go
+++ user-service-grpc/cmd/main.go
@@ -7,19 +7,21 @@ import (
 	"github.com/nsaltun/user-service-grpc/internal/service"
 	"github.com/nsaltun/user-service-grpc/proto/generated/go/userapi/v1"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
+	listener, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatalf("Failed to listen. %v", err)
+	}
+
 	server := grpc.NewServer()
 
 	//Register userapi to server
 	userService := service.NewUserAPI()
 	userapi.RegisterUserAPIServer(server, userService)
-
-	listener, err := net.Listen("tcp", ":50051")
-	if err != nil {
-		log.Fatalf("Failed to listen. %v", err)
-	}
+	reflection.Register(server)
 
 	if err := server.Serve(listener); err != nil {
 		log.Fatalf("Failed to serve. %v", err)
